3aee4d0637cba8b7b713df9bb2a857cf13a326cb
Quinn Keenan <kqfall1@gmail.com>
2025-12-15 10:53:52 -0500

    Final touches before beginning GameEngine.java state methods.

--- src/main/java/com/github/kqfall1/java/blackjackEngine/controllers/GameEngine.java
+++ src/main/java/com/github/kqfall1/java/blackjackEngine/controllers/GameEngine.java
@@ -1,6 +1,11 @@
 package com.github.kqfall1.java.blackjackEngine.controllers;
 
+import com.github.kqfall1.java.blackjackEngine.model.betting.Bet;
+import com.github.kqfall1.java.blackjackEngine.model.betting.Pot;
 import com.github.kqfall1.java.blackjackEngine.model.entities.*;
+import com.github.kqfall1.java.utils.LoggerUtils;
+import java.io.IOException;
+import java.util.logging.Logger;
 
 /**
  * Orchestrates the entire blackjack library by continuously processing input
@@ -15,15 +20,26 @@ public class GameEngine
 {
 	private final RuleConfig config;
 	private final Dealer dealer;
+	private Bet insuranceBet;
+	private final Pot insurancePot;
+	private final Logger logger;
+	private final Pot mainPot;
 	private final Player player;
 	private GameState state;
 
-	public GameEngine(RuleConfig config)
+	public GameEngine(RuleConfig config, String loggerFilePath,
+					  String loggerName) throws IOException
 	{
 		assert config != null : "config == null";
 		this.config = config;
-		this.dealer = new Dealer();
-		this.player = new Player();
+		dealer = new Dealer();
+		insurancePot = new Pot();
+		assert loggerFilePath != null : "loggerFilePath == null";
+		assert loggerName != null : "loggerName == null";
+		logger = LoggerUtils.newFileLogger(loggerFilePath, loggerName,
+			true);
+		mainPot = new Pot();
+		player = new Player();
 		setState(GameState.START);
 	}
 
@@ -37,6 +53,26 @@ public class GameEngine
 		return dealer;
 	}
 
+	public Bet getInsuranceBet()
+	{
+		return insuranceBet;
+	}
+
+	public Pot getInsurancePot()
+	{
+		return insurancePot;
+	}
+
+	public Logger getLogger()
+	{
+		return logger;
+	}
+
+	public Pot getMainPot()
+	{
+		return mainPot;
+	}
+
 	public Player getPlayer()
 	{
 		return player;
@@ -45,6 +81,12 @@ public class GameEngine
 	public GameState getState()
 	{
 		return state;
+	}
+
+	private void setInsuranceBet(Bet insuranceBet)
+	{
+		assert insuranceBet != null : "insuranceBet == null";
+		this.insuranceBet = insuranceBet;
 	}
 
 	private void setState(GameState state)
@@ -57,10 +99,14 @@ public class GameEngine
 	public String toString()
 	{
 		return String.format(
-			"%s[config=%s,dealer=%s,player=%s,state=%s]",
+			"%s[config=%s,dealer=%s,insuranceBet=%s,insurancePot=%s,logger=%s,mainPot=%s,player=%s,state=%s]",
 			getClass().getName(),
 			getConfig(),
 			getDealer(),
+			getInsuranceBet() != null ? getInsuranceBet() : "null",
+			getInsurancePot(),
+			getLogger(),
+			getMainPot(),
 			getPlayer(),
 			getState()
 		);
