7a22dd5d0f522385914ca2f76dad4b5707b43bcc
kqfall1 <kqfall1@gmail.com>
2026-04-16 22:14:52 -0400

    Began reading data from sources.md for CreditsJPanel.java.

--- src/main/java/com/github/kqfall1/java/blackjackEngine/view/swingApplication/jcomponents/CreditsJPanel.java
+++ src/main/java/com/github/kqfall1/java/blackjackEngine/view/swingApplication/jcomponents/CreditsJPanel.java
@@ -1,5 +1,10 @@
 package com.github.kqfall1.java.blackjackEngine.view.swingApplication.jcomponents;
 
+import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.util.Scanner;
 import javax.swing.*;
 
 /**
@@ -10,5 +15,34 @@ import javax.swing.*;
  */
 public final class CreditsJPanel extends JPanel
 {
+    private final JLabel sourcesJLabel;
 
+    public CreditsJPanel()
+    {
+        var sourcesString = "<ul>";
+
+        try (InputStream in = CreditsJPanel.class.getResourceAsStream("/sources.md"))
+        {
+            final var scanner = new Scanner(in);
+
+            while (scanner.hasNextLine())
+            {
+                var sourceStringFormat = "%s<li>%s</li>";
+
+                if (!scanner.hasNextLine())
+                {
+                    sourceStringFormat = String.format("%s</ul>", sourceStringFormat);
+                }
+
+                sourcesString = String.format(sourceStringFormat, sourcesString, scanner.nextLine().trim());
+            }
+        }
+        catch (IOException e)
+        {
+            throw new UncheckedIOException(e);
+        }
+
+        sourcesJLabel = new JLabel(sourcesString);
+        add(sourcesJLabel, BorderLayout.CENTER);
+    }
 }
